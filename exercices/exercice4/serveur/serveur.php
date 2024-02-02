593f7bf9981a7564a3cdcc9b295b6dbf28fa5bf5
rafaelCuendet <rafael.cuendetdecarvalho@studentfr.ch>
2024-02-02 10:53:35 +0100

    Ex 4 finis et fonctionnel

---
+++ exercices/exercice4/serveur/serveur.php
@@ -0,0 +1,9 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+			echo '<equipes>';
+			echo '<equipe><id>3</id><nom>SC Bern</nom></equipe>';
+			echo '<equipe><id>4</id><nom>Fribourg-Gottéron</nom></equipe>';
+			echo '<equipe><id>7</id><nom>HC Davos</nom></equipe>';
+			echo '<equipe><id>8</id><nom>Genève-Servette</nom></equipe>';
+			echo '</equipes>';
+?>
